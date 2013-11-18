338425c945506caa5aaa59545102031cc8b15691
Jaime Pulido Fentanes <jaime@Hewley>
2013-11-18 19:57:44 +0000

    Improved management of tweets

--- strands_tweets/scripts/tweet_test.py
+++ strands_tweets/scripts/tweet_test.py
@@ -10,7 +10,7 @@ def tweet_client(text):
     rospy.wait_for_service('/strands_tweets/Tweet')
     try:
         tweet = rospy.ServiceProxy('/strands_tweets/Tweet', Tweet)
-        resp1 = tweet(text,False)
+        resp1 = tweet(text,True)
         return resp1.result
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
