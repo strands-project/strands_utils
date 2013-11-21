b6f7ae3f4d315c3a0015cb7c3d05405370255bcf
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2013-11-21 13:56:35 +0000

    Added single report at once and updated list of achievements.

--- strands_marathon_achievements/scripts/achievement_monitor.py
+++ strands_marathon_achievements/scripts/achievement_monitor.py
@@ -58,6 +58,9 @@ def handle_achievement(achievements_db, type, current_value, target_value, achie
 	if existing == None:
 		broadcase_achievement(type, current_value, target_value, achievement)
 		achievements_db.insert(achievement_doc)
+		return True
+	else:
+		return False
 
 
 def create_test_val(achievement_type, episode):
@@ -69,7 +72,7 @@ def create_test_val(achievement_type, episode):
 		# hours
 		test_val = (latest.stamped_mileage[-1][1].total_seconds()/60)/60
 	elif achievement_type == 'distance':
-		# km
+		# m
 		test_val = latest.stamped_mileage[-1][0]
 	else:
 		try:
@@ -109,7 +112,7 @@ if __name__ == '__main__':
 		speech_service = rospy.ServiceProxy('/ros_mary', ros_mary_tts.srv.ros_mary)  
 
 
-	rate = rospy.Rate(0.1) # in hz -- summary is only updated every 5 minutes 
+	rate = rospy.Rate(1./360) # in hz -- summary is only updated every 5 minutes 
 	while not rospy.is_shutdown():
 
 		# load params
@@ -122,16 +125,24 @@ if __name__ == '__main__':
 		# get duration from last entry
 		# print latest.stamped_mileage[-1][1]
 
+		achievement_annouced = False
+
 		for achievement_type in achievement_list:
 
-			test_val = create_test_val(achievement_type, latest)
+			if not achievement_annouced:
+				test_val = create_test_val(achievement_type, latest)
 			
-			achievement_targets = achievement_list[achievement_type]
+				achievement_targets = achievement_list[achievement_type]
 
-			for achievement_target in achievement_targets:
+				for achievement_target in achievement_targets:
 
-				rospy.logdebug("is %s >= %s : %s", test_val, achievement_target['val'], test_val >= achievement_target['val'])
-				if test_val >= achievement_target['val']:
-					handle_achievement(achievements_db, achievement_type, test_val, achievement_target['val'], achievement_target['achievement'])
+					rospy.loginfo("is %s >= %s : %s", test_val, achievement_target['val'], test_val >= achievement_target['val'])
+					if test_val >= achievement_target['val']:
+						if handle_achievement(achievements_db, achievement_type, test_val, achievement_target['val'], achievement_target['achievement']):
+							achievement_annouced = True
+							break
+			else:
+				print "ignoring other achievements now"
+					
 
 		rate.sleep()
