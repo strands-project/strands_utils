4ed19438a330561a5ee9116a7197b2c5ddc5e942
Nils Bore <nbore@kth.se>
2014-04-02 12:02:04 +0200

    Added the ros wrapper package for ransac_primitives

---
+++ primitive_extraction/src/extraction.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_ros/point_cloud.h>
+
+#include "primitive_extractor.h"
+#include "plane_primitive.h"
+#include "sphere_primitive.h"
+#include "cylinder_primitive.h"
+
+#include "primitive_extraction/Primitive.h"
+#include "primitive_extraction/PrimitiveArray.h"
+
+#include <Eigen/Dense>
+
+ros::Publisher pub;
+primitive_params params;
+std::vector<base_primitive*> primitives;
+
+void write_plane_msg(primitive_extraction::Primitive& msg, const Eigen::VectorXd& data, std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> >& points)
+{
+    msg.type = "plane";
+    msg.pose.position.x = data(6);
+    msg.pose.position.y = data(7);
+    msg.pose.position.z = data(8);
+    msg.pose.orientation.x = data(9);
+    msg.pose.orientation.y = data(10);
+    msg.pose.orientation.z = data(11);
+    msg.pose.orientation.w = data(12);
+    msg.params.resize(6);
+    msg.params[0] = data(4);
+    msg.params[1] = data(5);
+    msg.params[2] = data(0);
+    msg.params[3] = data(1);
+    msg.params[4] = data(2);
+    msg.params[5] = data(3);
+    msg.points.resize(points.size());
+    for (int i = 0; i < points.size(); ++i) {
+        msg.points[i].x = points[i](0);
+        msg.points[i].y = points[i](1);
+        msg.points[i].z = points[i](2);
+    }
+}
+
+void write_cylinder_msg(primitive_extraction::Primitive& msg, const Eigen::VectorXd& data)
+{
+    msg.type = "cylinder";
+    msg.pose.position.x = data(3);
+    msg.pose.position.y = data(4);
+    msg.pose.position.z = data(5);
+    Eigen::Vector3d x = data.segment<3>(0);
+    Eigen::Vector3d y(-x(1), x(0), 0);
+    y.normalize();
+    Eigen::Vector3d z = x.cross(y);
+    z.normalize();
+    Eigen::Matrix3d R;
+    R.col(0) = x;
+    R.col(1) = y;
+    R.col(2) = z;
+    Eigen::Quaterniond quat(R);
+    msg.pose.orientation.x = quat.x();
+    msg.pose.orientation.y = quat.y();
+    msg.pose.orientation.z = quat.z();
+    msg.pose.orientation.w = quat.w();
+    msg.params.resize(2);
+    msg.params[0] = data(6); // radius
+    msg.params[1] = data(7); // height
+}
+
+void write_sphere_msg(primitive_extraction::Primitive& msg, const Eigen::VectorXd& data)
+{
+    msg.type = "sphere";
+    msg.pose.position.x = data(0);
+    msg.pose.position.y = data(1);
+    msg.pose.position.z = data(2);
+    Eigen::Quaterniond quat;
+    quat.setIdentity();
+    msg.pose.orientation.x = quat.x();
+    msg.pose.orientation.y = quat.y();
+    msg.pose.orientation.z = quat.z();
+    msg.pose.orientation.w = quat.w();
+    msg.params.resize(1);
+    msg.params[0] = data(3); // radius
+}
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    pcl::fromROSMsg(*msg, *cloud);
+
+    primitive_extractor extractor(cloud, primitives, params, NULL);
+    std::vector<base_primitive*> extracted;
+    extractor.extract(extracted);
+
+    std::cout << "The algorithm has finished..." << std::endl;
+    
+    primitive_extraction::PrimitiveArray msg_array;
+    msg_array.primitives.resize(extracted.size());
+    msg_array.camera_frame = msg->header.frame_id;
+    
+    for (int i = 0; i < extracted.size(); ++i) {
+        primitive_extraction::Primitive msg;
+        Eigen::VectorXd data;
+        std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > points;
+        extracted[i]->shape_data(data);
+        switch (extracted[i]->get_shape()) {
+        case base_primitive::PLANE: 
+            extracted[i]->shape_points(points);
+            write_plane_msg(msg, data, points);
+            break;
+        case base_primitive::CYLINDER:
+            write_cylinder_msg(msg, data);
+            break;
+        case base_primitive::SPHERE:
+            write_sphere_msg(msg, data);
+            break;
+        }
+        msg_array.primitives[i] = msg;
+    }
+    
+    pub.publish(msg_array);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "extraction");
+    // Initialize node parameters from launch file or command line.
+    // Use a private node handle so that multiple instances of the node can be run simultaneously
+    // while using different parameters.
+	ros::NodeHandle n;
+	
+	/*params.octree_res = 0.04;
+    params.normal_neigbourhood = 0.015;
+    params.inlier_threshold = 0.015;
+    params.angle_threshold = 0.4;
+    params.add_threshold = 0.01;
+    params.min_shape = 3000;
+    params.inlier_min = params.min_shape;
+    params.connectedness_res = 0.01;
+    params.distance_threshold = 2.0;*/
+    ros::NodeHandle pn("~");
+    pn.param<int>("number_disjoint_subsets", params.number_disjoint_subsets, 20);
+    pn.param<double>("octree_leaf_size", params.octree_res, 0.5);
+    pn.param<double>("normal_neigbourhood", params.normal_neigbourhood, 0.04);
+    pn.param<double>("inlier_threshold", params.inlier_threshold, 0.04);
+    pn.param<double>("angle_threshold", params.angle_threshold, 0.4);
+    pn.param<double>("error_add_probability", params.add_threshold, 0.01);
+    pn.param<int>("min_inliers", params.inlier_min, 2000);
+    pn.param<int>("min_terminate", params.min_shape, 2000);
+    pn.param<double>("connectedness_dist", params.connectedness_res, 0.03);
+    pn.param<double>("distance_threshold", params.distance_threshold, 4.0);
+    bool extract_planes, extract_cylinders, extract_spheres;
+    pn.param<bool>("extract_planes", extract_planes, true);
+    pn.param<bool>("extract_cylinders", extract_cylinders, true);
+    pn.param<bool>("extract_spheres", extract_spheres, true);
+    std::string input;
+    pn.param<std::string>("input", input, std::string("/head_xtion/depth_registered/points"));
+    std::string output;
+    pn.param<std::string>("output", output, std::string("/primitives"));
+    
+    if (extract_planes) {
+        primitives.push_back(new plane_primitive());
+    }
+    if (extract_cylinders) {
+        primitives.push_back(new cylinder_primitive());
+    }
+    if (extract_spheres) {
+        primitives.push_back(new sphere_primitive());
+    }
+	
+	ros::Subscriber sub = n.subscribe(input, 1, callback);
+	pub = n.advertise<primitive_extraction::PrimitiveArray>(output, 1);
+    
+    ros::spin();
+    
+    return 0;
+}
+
---
+++ primitive_extraction/src/primitives_to_markers.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+
+#include "ros/ros.h"
+
+#include "primitive_extraction/Primitive.h"
+#include "primitive_extraction/PrimitiveArray.h"
+
+#include "visualization_msgs/Marker.h"
+#include "visualization_msgs/MarkerArray.h"
+
+#include <Eigen/Dense>
+
+ros::Publisher pub;
+int previous_n;
+bool display_plane_box;
+double plane_r;
+double plane_g;
+double plane_b;
+
+void write_plane_marker(visualization_msgs::Marker& marker, const primitive_extraction::Primitive& primitive)
+{
+    if (display_plane_box) {
+        marker.type = visualization_msgs::Marker::CUBE;
+        marker.pose.position.x = primitive.pose.position.x;
+        marker.pose.position.y = primitive.pose.position.y;
+        marker.pose.position.z = primitive.pose.position.z;
+        marker.pose.orientation.x = primitive.pose.orientation.x;
+        marker.pose.orientation.y = primitive.pose.orientation.y;
+        marker.pose.orientation.z = primitive.pose.orientation.z;
+        marker.pose.orientation.w = primitive.pose.orientation.w;
+        marker.scale.x = 0.01;
+        marker.scale.y = primitive.params[0];
+        marker.scale.z = primitive.params[1];
+    }
+    else {
+        marker.type = visualization_msgs::Marker::LINE_STRIP;
+        marker.pose.position.x = 0.0;
+        marker.pose.position.y = 0.0;
+        marker.pose.position.z = 0.0;
+        Eigen::Quaterniond quat;
+        // these markers are in the camera's frame of reference
+        quat.setIdentity();
+        marker.pose.orientation.x = quat.x();
+        marker.pose.orientation.y = quat.y();
+        marker.pose.orientation.z = quat.z();
+        marker.pose.orientation.w = quat.w();
+        marker.scale.x = 0.02;
+        marker.points.resize(2*primitive.points.size());
+        for (int i = 0; i < primitive.points.size()-1 ; ++i) {
+            marker.points[2*i+2] = primitive.points[i];
+            marker.points[2*i+3] = primitive.points[i+1];
+        }
+        marker.points[0] = primitive.points[primitive.points.size() - 1];
+        marker.points[1] = primitive.points[0];
+    }
+    
+    marker.color.a = 1.0;
+    marker.color.r = plane_r;
+    marker.color.g = plane_g;
+    marker.color.b = plane_b;
+    
+}
+
+void write_cylinder_marker(visualization_msgs::Marker& marker, const primitive_extraction::Primitive& primitive)
+{
+    marker.type = visualization_msgs::Marker::CYLINDER;
+    marker.pose.position.x = primitive.pose.position.x;
+    marker.pose.position.y = primitive.pose.position.y;
+    marker.pose.position.z = primitive.pose.position.z;
+    Eigen::Quaterniond q;
+    q.x() = primitive.pose.orientation.x;
+    q.y() = primitive.pose.orientation.y;
+    q.z() = primitive.pose.orientation.z;
+    q.w() = primitive.pose.orientation.w;
+    Eigen::Quaterniond t(Eigen::AngleAxisd(M_PI/2.0, Eigen::Vector3d::UnitY()));
+    q *= t;
+    marker.pose.orientation.x = q.x();
+    marker.pose.orientation.y = q.y();
+    marker.pose.orientation.z = q.z();
+    marker.pose.orientation.w = q.w();
+    marker.scale.x = 2.0*primitive.params[0];
+    marker.scale.y = 2.0*primitive.params[0];
+    marker.scale.z = primitive.params[1];
+    marker.color.a = 1.0;
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+}
+
+void write_sphere_marker(visualization_msgs::Marker& marker, const primitive_extraction::Primitive& primitive)
+{
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.pose.position.x = primitive.pose.position.x;
+    marker.pose.position.y = primitive.pose.position.y;
+    marker.pose.position.z = primitive.pose.position.z;
+    marker.pose.orientation.x = primitive.pose.orientation.x;
+    marker.pose.orientation.y = primitive.pose.orientation.y;
+    marker.pose.orientation.z = primitive.pose.orientation.z;
+    marker.pose.orientation.w = primitive.pose.orientation.w;
+    marker.scale.x = 2.0*primitive.params[0];
+    marker.scale.y = 2.0*primitive.params[0];
+    marker.scale.z = 2.0*primitive.params[0];
+    marker.color.a = 1.0;
+    marker.color.r = 0.0;
+    marker.color.g = 0.0;
+    marker.color.b = 1.0;
+}
+
+void callback(const primitive_extraction::PrimitiveArray::ConstPtr& msg)
+{
+    int n = msg->primitives.size();
+    std::string camera_frame = msg->camera_frame;
+    visualization_msgs::MarkerArray markers;
+    
+    if (n >= previous_n) {
+        markers.markers.resize(n);
+    }
+    else {
+        markers.markers.resize(previous_n);
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        visualization_msgs::Marker marker;
+        marker.header.frame_id = camera_frame;
+        marker.header.stamp = ros::Time();
+        marker.ns = "my_namespace"; // what's this for?
+        marker.id = i + 1;
+        marker.action = visualization_msgs::Marker::ADD;
+        std::string primitive_type = msg->primitives[i].type;
+        if (primitive_type == "plane") {
+            write_plane_marker(marker, msg->primitives[i]);
+        }
+        else if (primitive_type == "cylinder") {
+            write_cylinder_marker(marker, msg->primitives[i]);
+        }
+        else if (primitive_type == "sphere") {
+            write_sphere_marker(marker, msg->primitives[i]);
+        }
+        markers.markers[i] = marker;
+    }
+    
+    for (int i = n; i < previous_n; ++i) {
+        markers.markers[i].action = visualization_msgs::Marker::DELETE;
+        markers.markers[i].header.frame_id = camera_frame;
+        markers.markers[i].header.stamp = ros::Time();
+        markers.markers[i].ns = "my_namespace"; // what's this for?
+        markers.markers[i].id = i + 1;
+    }
+    
+    previous_n = n;
+    pub.publish(markers);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "primitives_to_markers");
+    ros::NodeHandle n;
+    
+    // Initialize node parameters from launch file or command line.
+    // Use a private node handle so that multiple instances of the node can be run simultaneously
+    // while using different parameters.
+	ros::NodeHandle pn("~");
+	std::string input;
+	pn.param<std::string>("input", input, std::string("/primitives"));
+	std::string output;
+	pn.param<std::string>("output", output, std::string("/primitive_marker_array"));
+	pn.param<bool>("display_plane_box", display_plane_box, false);
+	pn.param<double>("plane_r", plane_r, 1.0);
+	pn.param<double>("plane_g", plane_g, 0.0);
+	pn.param<double>("plane_b", plane_b, 0.0);
+	
+	ros::Subscriber sub = n.subscribe(input, 1, callback);
+	pub = n.advertise<visualization_msgs::MarkerArray>(output, 1);
+	
+	previous_n = 0;
+    
+    ros::spin();
+    
+    return 0;
+}
+
