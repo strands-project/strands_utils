847183abe394bc0831349918173dec14f5b1982d
Jaime Pulido Fentanes <jaime@Hewley>
2014-04-15 14:59:00 +0100

    bug fix

--- laser_door_check/script/door_check.py
+++ laser_door_check/script/door_check.py
@@ -16,44 +16,49 @@ class DoorCheck(object):
 
     def __init__(self, name):
         self._action_name = name
-        print "Starting Action Server"
+        rospy.loginfo('Starting Action Server')
         self._as = actionlib.SimpleActionServer(self._action_name, laser_door_check.msg.DoorCheckAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
-        print "Done"
+        rospy.loginfo('Done')
     
     def execute_cb(self, goal):
         # publish info to the console for the user
         self._laser_received=False
-        rospy.loginfo('%s: Executing, checking door at %i m' % (self._action_name, goal.distance))
-        self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
-        # start executing the action
-        timeout=0
-        while (not self._laser_received) and timeout < 100:
-            sleep(0.1)
-            timeout=timeout+1
-        
-        if self._laser_received :
-            increment = (self.angle_max - self.angle_min)/ float(len(self._laser_data))
-            start_angle = math.atan(0.4/goal.distance)
-            start_sample = int(len(self._laser_data)-math.floor((start_angle-self.angle_min) / increment))
-            final_sample = int(len(self._laser_data)-math.floor((self.angle_max-start_angle) / increment))
-            i=start_sample
-            self._result.open=True            
-            while i<= final_sample and self._result.open :
-                if self._laser_data[i] < goal.distance :
-                    self._result.open = False
-                i= i+1
-            print self._result
-            print self._laser_data
-            self._as.set_succeeded(self._result)
-        else :
-            print "no laser data"
+        if goal.distance <= 0 :
+            rospy.loginfo('distance has to be greater than 0')
             self._result.open=False
             self._as.set_succeeded(self._result)
+        else :
+            rospy.loginfo('%s: Executing, checking door at %i m' % (self._action_name, goal.distance))
+            self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+            # start executing the action
+            timeout=0
+            while (not self._laser_received) and timeout < 100:
+                sleep(0.1)
+                timeout=timeout+1
+            
+            if self._laser_received :
+                increment = (self.angle_max - self.angle_min)/ float(len(self._laser_data))
+                start_angle = math.atan(0.4/goal.distance)
+                start_sample = int(len(self._laser_data)-math.floor((start_angle-self.angle_min) / increment))
+                final_sample = int(len(self._laser_data)-math.floor((self.angle_max-start_angle) / increment))
+                i=start_sample
+                self._result.open=True            
+                while i<= final_sample and self._result.open :
+                    if self._laser_data[i] < goal.distance :
+                        self._result.open = False
+                    i= i+1
+                #print self._result
+                #print self._laser_data
+                self._as.set_succeeded(self._result)
+            else :
+                print "no laser data"
+                self._result.open=False
+                self._as.set_succeeded(self._result)
     
     def laser_callback(self, data):
         #rospy.loginfo(rospy.get_name() + ": I heard %s" % data.charging)
-        print ('%f to %f at %f/%d' %(data.angle_min, data.angle_max, data.angle_increment, len(data.ranges)))
+        #print ('%f to %f at %f/%d' %(data.angle_min, data.angle_max, data.angle_increment, len(data.ranges)))
         self.angle_min = data.angle_min
         self.angle_max= data.angle_max
         #selfdata.angle_increment, len(data.ranges)
