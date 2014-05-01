2027428bb8154b287173661110dcff33259a5708
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-05-01 16:43:22 +0100

    Added speaking of result.

--- check_something/scripts/check_door.py
+++ check_something/scripts/check_door.py
@@ -4,18 +4,32 @@ import actionlib
 from check_something.checker import Checker
 from strands_navigation_msgs.msg import *
 from actionlib_msgs.msg import GoalStatus
-
+from ros_mary_tts.msg import *
 
 class DoorChecker(Checker):
 	def __init__(self): 
 		super( DoorChecker, self ).__init__('check_door', 'doorDetection', DoorCheckAction)
+		self.speech_client = None
+
+	def speak(self, text):
+		if self.speech_client == None:
+			action = 'speak'
+			rospy.loginfo('waiting for %s server' % (action))
+			self.speech_client = actionlib.SimpleActionClient(action, maryttsAction)
+			self.speech_client.wait_for_server(rospy.Duration(10))
+
+		if self.speech_client != None:
+			goal = maryttsGoal(text)
+			self.speech_client.send_goal_and_wait(goal, rospy.Duration(20))
+
+		rospy.loginfo(text)
 
 	def generate_report(self, state, result):	
 		if state == GoalStatus.SUCCEEDED:
 			if result.open:
-				rospy.loginfo("Door is open")
+				self.speak("Door is open")
 			else:
-				rospy.loginfo("Door is not open")
+				self.speak("Door is closed")
 		else:	
 			rospy.loginfo("Action interrupted or aborted")
 
