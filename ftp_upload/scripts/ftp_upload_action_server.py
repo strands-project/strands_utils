b68da07c1580e828444943a9f4a8c0c675436d47
Rares Ambrus <raambrus@kth.se>
2014-05-15 14:51:32 +0200

    Fixed action message import error

--- ftp_upload/scripts/ftp_upload_action_server.py
+++ ftp_upload/scripts/ftp_upload_action_server.py
@@ -11,21 +11,29 @@ class FTPUploadServer:
   def __init__(self):
     rospy.init_node('ftp_upload_actionserver')
 
-    self.server = actionlib.SimpleActionServer('ftp_upload', FtpUploadAction, self.execute, False)
+    self.server = actionlib.SimpleActionServer('ftp_upload', ftp_upload.msg.FtpUploadAction, self.execute, False)
     self.server.register_preempt_callback(self.preemptCallback)
     self.server.start()
-    rospy.loginfo('Ptu action server started')
+    rospy.loginfo('Ftp upload server started')
 
     self.cancelled = False
 
-    self.feedback = FtpUploadFeedback()
-    self.result = FtpUploadResult()
+    self.feedback = ftp_upload.msg.FtpUploadFeedback()
+    self.result = ftp_upload.msg.FtpUploadResult()
     self.result.success = True
 
 
   def execute(self, goal):
 
 	print 'Starting FTP upload to server ',goal.ftp_server
+
+    	if self.cancelled:
+                self.result.success = False
+                self.server.set_preempted(self.result)
+	else:
+                self.result.success = True
+                self.server.set_succeeded(self.result)
+
 
   def preemptCallback(self):
 	self.cancelled = True
