7e0870a5920170ea6e956fa437cfe9feb30f6341
Nils Bore <nbore@kth.se>
2014-08-12 15:01:41 +0200

    Added the cylinders in the new framework

--- ransac_primitives/src/cylinder_primitive.cpp
+++ ransac_primitives/src/cylinder_primitive.cpp
@@ -21,7 +21,7 @@ int cylinder_primitive::points_required()
 }
 
 bool cylinder_primitive::construct(const MatrixXd& points, const MatrixXd& normals,
-                                double inlier_threshold, double angle_threshold)
+                                   double inlier_threshold, double angle_threshold)
 {
     Vector3d p1 = points.col(0);
     Vector3d p2 = points.col(1);
@@ -75,73 +75,80 @@ bool cylinder_primitive::construct(const MatrixXd& points, const MatrixXd& norma
 void cylinder_primitive::compute_inliers(std::vector<int>& inliers, const MatrixXd& points, const MatrixXd& normals,
                                          const std::vector<int>& inds, double inlier_threshold, double angle_threshold)
 {
-    min2 << INFINITY, -r*M_PI;
-    max2 << -INFINITY, r*M_PI;
-
     Vector3d pt;
     Vector3d n;
     Vector2d rad;
-    Vector2d pt2;
-    std::vector<int> temp;
-    std::vector<Vector2d, aligned_allocator<Eigen::Vector2d> > plane_pts;
     for (const int& i : inds) {
         pt = points.col(i);
         n = normals.col(i);
         rad = basis.transpose()*(pt - c);
         if (fabs(rad.norm() - r) < inlier_threshold && M_PI/2.0 - acos(fabs(a.dot(n))) < angle_threshold) {
-            pt2(0) = a.dot(pt - c);
-            pt2(1) = r*atan2(rad(0), rad(1));
-            if (pt2(0) < min2(0)) {
-                min2(0) = pt2(0);
-            }
-            if (pt2(0) > max2(0)) {
-                max2(0) = pt2(0);
-            }
-            temp.push_back(i);
-            plane_pts.push_back(pt2);
+            inliers.push_back(i);
         }
     }
+}
 
-    if (inlier_refinement == 1 && int(temp.size()) < min_inliers) {
-        return;
-    }
+void cylinder_primitive::largest_connected_component(std::vector<int>& inliers, const MatrixXd& points)
+{
+    Vector2i minpt;
+    minpt << INT32_MAX, int(-r*M_PI/current_connectedness_res());
+    Vector2i maxpt;
+    maxpt << -INT32_MAX, int(r*M_PI/current_connectedness_res());
 
-    max2(0) += connectedness_res/2.0;
-    min2(0) -= connectedness_res/2.0;
-    Vector2d size2 = max2 - min2;
-    int width = int(int(size2(0)/connectedness_res)) + 1;
-    int height = int(int(size2(1)/connectedness_res)) + 1;
-    cv::Mat binary = cv::Mat::zeros(height, width, CV_32SC1);
-    cv::Mat binary1 = cv::Mat::zeros(height, width, CV_32SC1);
-
-    std::vector<Vector2i, aligned_allocator<Eigen::Vector2i> > plane_ptsi;
-    plane_ptsi.resize(plane_pts.size());
-
+    Vector3d pt;
+    Vector2d rad;
+    Vector2i pt2;
+    std::vector<int> temp;
+    std::vector<Vector2i, aligned_allocator<Eigen::Vector2i> > plane_pts;
+    plane_pts.resize(conforming_inds.size());
     int counter = 0;
-    Vector2i pt2i;
-    for (const Vector2d& pp : plane_pts) {
-        pt2i(0) = int((pp(0) - min2(0))/connectedness_res);
-        pt2i(1) = int((pp(1) - min2(1))/connectedness_res);
-        binary.at<int>(pt2i(1), pt2i(0)) = 1;
-        binary1.at<int>(pt2i(1), pt2i(0)) = 65535;
-        plane_ptsi[counter] = pt2i;
+    for (const int& i : conforming_inds) {
+        pt = points.col(i);
+        rad = basis.transpose()*(pt - c);
+        pt2(0) = int(a.dot(pt - c)/current_connectedness_res());
+        pt2(1) = int(r*atan2(rad(0), rad(1))/current_connectedness_res());
+        if (pt2(0) < minpt(0)) {
+            minpt(0) = pt2(0);
+        }
+        if (pt2(0) > maxpt(0)) {
+            maxpt(0) = pt2(0);
+        }
+        temp.push_back(i);
+        plane_pts[counter] = pt2;
         ++counter;
     }
 
-    //cv::imshow("Binary1", binary1);
+    int width = 1 + maxpt(0) - minpt(0);
+    int height = 1 + maxpt(1) - minpt(1);
+
+    if (width < 10) {
+        inliers = conforming_inds;
+        return;
+    }
+
+    cv::Mat binary = cv::Mat::zeros(height, width, CV_32SC1);
+    //cv::Mat binary0 = cv::Mat::zeros(height, width, CV_32SC1);
+
+    for (Vector2i& pp : plane_pts) {
+        pp -= minpt;
+        binary.at<int>(pp(1), pp(0)) = 1;
+        //binary0.at<int>(pp(1), pp(0)) = 65535;
+    }
+
+    //cv::imshow("Binary0", binary0);
     //cv::waitKey(0);
 
     cv::Mat support = cv::Mat::zeros(width, height, CV_8UC1);
-    cv::Mat binary2 = cv::Mat::zeros(height, width, CV_32SC1);
+    //cv::Mat binary2 = cv::Mat::zeros(height, width, CV_32SC1);
 
     inliers.reserve(temp.size());
     int largest = find_blobs(binary, true);
     counter = 0;
-    for (const Vector2i& pp : plane_ptsi) {
+    for (const Vector2i& pp : plane_pts) {
         if (binary.at<int>(pp(1), pp(0)) == largest) {
-            inliers.push_back(temp[counter]);
+            inliers.push_back(conforming_inds[counter]);
             support.at<unsigned char>(pp(0), pp(1)) = 1;
-            binary2.at<int>(pp(1), pp(0)) = 65535;
+            //binary2.at<int>(pp(1), pp(0)) = 65535;
         }
         ++counter;
     }
@@ -174,8 +181,7 @@ base_primitive* cylinder_primitive::instantiate()
 
 void cylinder_primitive::draw(boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer)
 {
-    return; // don't draw cylinders for now
-    pcl::ModelCoefficients mc;
+    /*pcl::ModelCoefficients mc;
     Vector3d tc = c + min2(0)*a;
     Vector3d ta = c + max2(0)*a - tc;
     mc.values.resize(7);
@@ -187,7 +193,7 @@ void cylinder_primitive::draw(boost::shared_ptr<pcl::visualization::PCLVisualize
     mc.values[5] = ta(2);
     mc.values[6] = r;
     viewer->addCylinder(mc, std::string("cylinder") + boost::lexical_cast<std::string>(cylinders_drawn));
-    ++cylinders_drawn;
+    ++cylinders_drawn;*/
 }
 
 double cylinder_primitive::distance_to_pt(const Vector3d& pt)
@@ -243,9 +249,4 @@ void cylinder_primitive::shape_data(VectorXd& data)
 void cylinder_primitive::shape_points(std::vector<Vector3d, aligned_allocator<Vector3d> >& points)
 {
     points.clear();
-}
-
-void cylinder_primitive::largest_connected_component(std::vector<int>& inliers, const MatrixXd& points)
-{
-
 }
